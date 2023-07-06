394f6cf610f87ddcab7a76936e73a05aea7d8bc1
ElizabethBeaton <elizabethbeaton23@outlook.com>
2023-07-06 22:19:22 +0100

    moved files over

---
+++ script.js
@@ -0,0 +1,118 @@
+const characters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "~",
+  "`",
+  "!",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "_",
+  "-",
+  "+",
+  "=",
+  "{",
+  "[",
+  "}",
+  "]",
+  ",",
+  "|",
+  ":",
+  ";",
+  "<",
+  ">",
+  ".",
+  "?",
+  "/",
+];
+
+let passwordLength = 12;
+
+function getRandomCharacter() {
+  let randomChar = Math.floor(Math.random() * characters.length);
+  return characters[randomChar];
+}
+
+function generateRandomPassword() {
+  let randomPassword = "";
+  for (let i = 0; i < passwordLength; i++) {
+    randomPassword += getRandomCharacter();
+  }
+  return randomPassword;
+}
+
+function updatePasswordFields() {
+  const passwordField1 = document.getElementById("password-1");
+  const passwordField2 = document.getElementById("password-2");
+  passwordField1.value = generateRandomPassword();
+  passwordField2.value = generateRandomPassword();
+}
+
+const generateButton = document.querySelector(".container_button button");
+generateButton.addEventListener("click", updatePasswordFields);
